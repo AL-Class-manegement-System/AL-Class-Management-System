63d47b4a7712a3aeced61facb9539f75e973173f
damsithdewmina2005-beep <damsithdewmina2005@gmail.com>
2025-11-26 21:52:50 +0530

    edite

--- lib/registrationbackend.php
+++ lib/registrationbackend.php
@@ -123,11 +123,5 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     header("Location:../log/registration.php");
     exit();
 }
-
-<<<<<<< HEAD
-// $conn->close();
-
-?>
-=======
 $conn->close();
->>>>>>> main
+?>
