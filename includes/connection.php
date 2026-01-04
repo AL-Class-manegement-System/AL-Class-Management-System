34412cb36f95ddffd44a2fcc178de36bbd57b0f2
devmina-ddh <damsithdewmina2005@gmail.com>
2026-01-05 00:48:44 +0530

    done

--- includes/connection.php
+++ includes/connection.php
@@ -1,27 +1,27 @@
 <?php
-    $dbserver = "localhost";
-    $dbuser = "root";
-    $password = "";
-    $database = "al_class_db";
+    // $dbserver = "localhost";
+    // $dbuser = "root";
+    // $password = "";
+    // $database = "al_class_db";
 
-    $conn = mysqli_connect($dbserver, $dbuser, $password, $database);
+    // $conn = mysqli_connect($dbserver, $dbuser, $password, $database);
     
-    if(!$conn){
-        die("Error " . mysqli_connect_error());
-    }
+    // if(!$conn){
+    //     die("Error " . mysqli_connect_error());
+    // }
 ?>
 
 <?php
-// $servername = "futuremindssite.site";
-// $username = "futuremi"; 
-// $password = "IQ4-ij.Sim33J2"; 
-// $dbname = "futuremi_al_class_db"; 
+$servername = "futuremindssite.site";
+$username = "futuremi"; 
+$password = "IQ4-ij.Sim33J2"; 
+$dbname = "futuremi_al_class_db"; 
 
-// $con = new mysqli($servername, $username, $password, $dbname);
+$con = new mysqli($servername, $username, $password, $dbname);
 
-// $con->set_charset("utf8mb4");
+$con->set_charset("utf8mb4");
 
-// if ($con->connect_error) {
-//     die("Connection failed: " . $con->connect_error);
-// }
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
 ?>
